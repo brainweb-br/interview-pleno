d0e4aaf2e792c48b1e92081d3712863ffd7063f5
Wilker <wilker.jose@morpheustecnologia.com>
2020-10-25 21:01:54 -0300

    desenvolvimento final

--- model/src/main/java/br/com/brainweb/interview/model/Hero.java
+++ model/src/main/java/br/com/brainweb/interview/model/Hero.java
@@ -1,4 +1,44 @@
 package br.com.brainweb.interview.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants(asEnum = true)
 public class Hero {
+
+    private UUID id;
+
+    @NotNull(message = "Power e obrigatório")
+    private UUID power;
+
+    @NotBlank(message = "Nome e obrigatório")
+    private String name;
+
+    @Pattern(regexp = "^(HUMAN|ALIEN|DIVINE|CYBORG)$",
+            message = "Race inválido")
+    private String race;
+
+    private Boolean enabled = Boolean.TRUE;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private OffsetDateTime createdAt;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private OffsetDateTime updatedAt;
+
 }
--- model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
+++ model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
@@ -1,4 +1,42 @@
 package br.com.brainweb.interview.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants(asEnum = true)
 public class PowerStats {
+
+    private UUID id;
+
+    @NotNull(message = "Strength e obrigatório")
+    private Long strength;
+
+    @NotNull(message = "Agility e obrigatório")
+    private Long agility;
+
+    @NotNull(message = "Dexterity e obrigatório")
+    private Long dexterity;
+
+    @NotNull(message = "Intelligence e obrigatório")
+    private Long intelligence;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime updatedAt;
+
 }
