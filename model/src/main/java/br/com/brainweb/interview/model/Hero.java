958a90dc062f0ba98eb3f08f46a7dd0f922c73d7
DAVI VASCONCELOS <davi.vasconcelos@portoseguro.com.br>
2021-05-08 12:28:50 -0300

    added RaceType Enum and validation of race

--- model/src/main/java/br/com/brainweb/interview/model/Hero.java
+++ model/src/main/java/br/com/brainweb/interview/model/Hero.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -13,12 +14,22 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Hero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String name;
-    private String race; //todo validation
+    @Enumerated(EnumType.STRING)
+    private RaceType race; //todo validation
     private Boolean enable;
     private Timestamp createdAt;
     private Timestamp updatedAt;
     private PowerStats powerStats;
+
+    public enum RaceType {
+        HUMAN, ALIEN, DIVINE, CYBORG
+    }
 }
+
+
