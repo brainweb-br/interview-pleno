976c5f516102c6542fab7e7be78fdf0f993e5f85
DAVI VASCONCELOS <davi.vasconcelos@portoseguro.com.br>
2021-05-08 15:04:01 -0300

    added create hero feature

--- core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroService.java
+++ core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroService.java
@@ -1,4 +1,25 @@
 package br.com.brainweb.interview.core.features.hero;
 
+import br.com.brainweb.interview.model.Hero;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
 public class HeroService {
+
+    @Autowired
+    private HeroRepository heroRepository;
+
+    public Hero createHero(Hero hero) throws ResponseStatusException {
+        try {
+            hero = heroRepository.save(hero);
+        } catch(IllegalArgumentException ex) {
+            // todo https://www.baeldung.com/spring-exceptions-json
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
+        return hero;
+    }
 }
