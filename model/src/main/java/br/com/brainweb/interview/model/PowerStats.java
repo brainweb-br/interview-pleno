767b8ddeb520e13a6112c91532ea59d14df0e90d
DAVI VASCONCELOS <davi.vasconcelos@portoseguro.com.br>
2021-05-08 12:38:39 -0300

    added power stats model

--- model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
+++ model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
@@ -1,4 +1,29 @@
 package br.com.brainweb.interview.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class PowerStats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private Integer strength;
+    private Integer agility;
+    private Integer intelligence;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 }
