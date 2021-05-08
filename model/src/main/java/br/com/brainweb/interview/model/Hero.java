67310a4158418e9acd09a7ac517029d97863373a
DAVI VASCONCELOS <davi.vasconcelos@portoseguro.com.br>
2021-05-08 12:11:48 -0300

    added hero model attrs

--- model/src/main/java/br/com/brainweb/interview/model/Hero.java
+++ model/src/main/java/br/com/brainweb/interview/model/Hero.java
@@ -1,4 +1,24 @@
 package br.com.brainweb.interview.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hero {
+    private UUID id;
+    private String name;
+    private String race; //todo validation
+    private Boolean enable;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private PowerStats powerStats;
 }
