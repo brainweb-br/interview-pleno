391eec62213a105f1c5322041fde88ed8ca3ae8d
Nivaldo <nivaldojunior.93@gmail.com>
2021-07-12 14:15:23 -0300

    modelando entidades

--- model/src/main/java/br/com/brainweb/interview/model/Hero.java
+++ model/src/main/java/br/com/brainweb/interview/model/Hero.java
@@ -1,4 +1,28 @@
 package br.com.brainweb.interview.model;
 
-public class Hero {
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Hero extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private UUID id;
+    @Column(unique=true)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Race race;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "power_stats_id")
+    private PowerStats powerStats;
+    private boolean enabled;
+
 }
--- model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
+++ model/src/main/java/br/com/brainweb/interview/model/PowerStats.java
@@ -1,4 +1,26 @@
 package br.com.brainweb.interview.model;
 
-public class PowerStats {
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class PowerStats extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private short strength;
+    private short agility;
+    private short dexterity;
+    private short intelligence;
+
 }
