a63bc17f645ad775736ad12a6b5c21e4a9e0dccf
pedroanhezini <pedroanhezini@hotmail.com>
2021-07-17 01:26:54 -0300

    Added Dockerfile configuration

--- core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroController.java
+++ core/src/main/java/br/com/brainweb/interview/core/features/hero/HeroController.java
@@ -1,4 +1,17 @@
 package br.com.brainweb.interview.core.features.hero;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/heroes")
 public class HeroController {
+
+    @GetMapping
+    public String hello(){
+        System.out.println("Hello, heroes.");
+        return "Hello, heroes.";
+    }
+
 }
